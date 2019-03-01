6ce61356c5d4666f308c78b40e30758b9a1fa688
Bernhard Stöcker <bernhard.stoecker@aktivweb.de>
2019-03-01 15:14:55 +0100

    added method for bulk insert or update operations

--- baserepository.h
+++ baserepository.h
@@ -51,6 +51,76 @@ protected :
 
         }
     }
+	
+	  /**
+     * bulk insert or update of many entity instances using one prepared statement
+     */
+    template <class T> void bulkSave(const QVector<shared_ptr<T>> & beans ) {
+        if(!beans.isEmpty()) {
+
+            for(const auto & b : beans) {
+                if (b->isInsertNew()){
+                    for(const auto & bean : beans) {
+                        if (bean->isInsertNew()){
+                            QString sql = QStringLiteral("INSERT INTO %1 (%2) VALUES (%3)").arg(b->getTableName(), b->getInsertFields(), b->getInsertValuePlaceholders());
+                            QSqlQuery q(sqlCon->getCon());
+                            bool res = q.prepare(sql);
+                            for(const auto & bean : beans) {
+                                QList<QVariant> params=bean->getInsertParams();
+                                for(int i = 0; i < params.size(); i++) {
+                                    q.addBindValue(params.at(i));
+
+                                }
+                                res &= q.exec();
+                                if (bean->isAutoIncrement()) {
+
+
+                                    int id = q.lastInsertId().toInt();
+                                    bean->setAutoIncrementId(id);
+                                }
+                                bean->setInsertNew(false);
+                            }
+
+                            if(!res) {
+                                throw SqlException(q.lastError().number(),q.lastError().text());
+                            }
+                        }
+                    }
+                    break;
+                }
+            }
+            for(const auto & b : beans) {
+                if (!b->isInsertNew()){
+                    for(const auto & bean : beans) {
+                        if (!bean->isInsertNew()){
+                            QList<QVariant> params;
+                            QString updateFields = bean->getUpdateFields(&params).join(QChar(','));
+
+                            if (params.size() > 0) {
+
+                                QString query = QStringLiteral("UPDATE %1 SET %2 WHERE %3").arg(bean->getTableName(),updateFields,bean->getUpdateCondition());
+                                QList<QVariant> conditionParams=  bean->getUpdateConditionParams();
+                                params.append(conditionParams);
+#ifdef QT_DEBUG
+                                qDebug() << query;
+#endif
+                                QSqlQuery q(sqlCon->getCon());
+                                bool res = q.prepare(query);
+                                for(int i = 0; i < params.size(); i++) {
+                                    q.addBindValue(params.at(i));
+
+                                }
+                                res &= q.exec();
+                            }
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+
+
+    }
 
     template <class T> void saveBean(const shared_ptr<T> & bean ) {
         if (bean->isInsertNew()){
